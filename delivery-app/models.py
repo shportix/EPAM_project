a3214dcf629d18f34976e35ad921ccc248894e54
DanilShporta <danylo.shporta@nure.ua>
2022-01-10 02:41:13 +0200

    fixes pylint errors in models

--- delivery-app/models.py
+++ delivery-app/models.py
@@ -11,7 +11,7 @@ class Position(db.Model):  # pylint: disable=too-few-public-methods
     """
     __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)  # pylint: disable=E1101
-    position_name = db.Column(db.String(100), unique=True)
+    position_name = db.Column(db.String(100), unique=True)  # pylint: disable=E1101
 
 
 class User(db.Model):  # pylint: disable=too-few-public-methods
@@ -19,13 +19,13 @@ class User(db.Model):  # pylint: disable=too-few-public-methods
     user table model
     """
     __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
-    name = db.Column(db.String(100), nullable=True)
-    surname = db.Column(db.String(100), nullable=True)
-    phoneNumber = db.Column(db.String(50), nullable=True)
-    isEmployee = db.Column(db.Boolean, default=False)
+    id = db.Column(db.Integer, primary_key=True)  # pylint: disable=E1101
+    email = db.Column(db.String(100), nullable=False, unique=True)  # pylint: disable=E1101
+    password = db.Column(db.String(100), nullable=False)  # pylint: disable=E1101
+    name = db.Column(db.String(100), nullable=True)  # pylint: disable=E1101
+    surname = db.Column(db.String(100), nullable=True)  # pylint: disable=E1101
+    phoneNumber = db.Column(db.String(50), nullable=True)  # pylint: disable=E1101
+    isEmployee = db.Column(db.Boolean, default=False)  # pylint: disable=E1101
 
 
 class Department(db.Model):  # pylint: disable=too-few-public-methods
@@ -33,10 +33,10 @@ class Department(db.Model):  # pylint: disable=too-few-public-methods
     Department table model
     """
     __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=True)
-    city = db.Column(db.String(100), nullable=False)
-    address = db.Column(db.String(200), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)  # pylint: disable=E1101
+    name = db.Column(db.String(100), nullable=True)  # pylint: disable=E1101
+    city = db.Column(db.String(100), nullable=False)  # pylint: disable=E1101
+    address = db.Column(db.String(200), nullable=False)  # pylint: disable=E1101
 
 
 class Employee(db.Model):  # pylint: disable=too-few-public-methods
@@ -44,11 +44,11 @@ class Employee(db.Model):  # pylint: disable=too-few-public-methods
     Employee table model
     """
     __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
-    position_id = db.Column(db.Integer, db.ForeignKey("position.id"), nullable=False)
-    department_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=True)
-    birthday = db.Column(db.Date, nullable=False)
-    salary = db.Column(db.Integer, nullable=False)
+    id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)  # pylint: disable=E1101
+    position_id = db.Column(db.Integer, db.ForeignKey("position.id"), nullable=False)  # pylint: disable=E1101
+    department_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=True)  # pylint: disable=E1101
+    birthday = db.Column(db.Date, nullable=False)  # pylint: disable=E1101
+    salary = db.Column(db.Integer, nullable=False)  # pylint: disable=E1101
 
 
 class Image(db.Model):  # pylint: disable=too-few-public-methods
@@ -56,10 +56,10 @@ class Image(db.Model):  # pylint: disable=too-few-public-methods
     image table model
     """
     __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    img = db.Column(db.Text)
-    name = db.Column(db.String(200), nullable=False)
-    mimetype = db.Column(db.String(200), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)  # pylint: disable=E1101
+    img = db.Column(db.Text)  # pylint: disable=E1101
+    name = db.Column(db.String(200), nullable=False)  # pylint: disable=E1101
+    mimetype = db.Column(db.String(200), nullable=False)  # pylint: disable=E1101
 
 
 class DishCategory(db.Model):  # pylint: disable=too-few-public-methods
@@ -67,8 +67,8 @@ class DishCategory(db.Model):  # pylint: disable=too-few-public-methods
     dish category table model
     """
     __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    category_name = db.Column(db.String(100), unique=True)
+    id = db.Column(db.Integer, primary_key=True)  # pylint: disable=E1101
+    category_name = db.Column(db.String(100), unique=True)  # pylint: disable=E1101
 
 
 class Dish(db.Model):  # pylint: disable=too-few-public-methods
@@ -77,12 +77,12 @@ class Dish(db.Model):  # pylint: disable=too-few-public-methods
     """
     __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
-    category_id = db.Column(db.Integer, db.ForeignKey('dish_category.id'))
-    dish_name = db.Column(db.String(100), unique=True, nullable=False)
-    cost_price = db.Column(db.Integer, nullable=False)
-    value_added = db.Column(db.Integer, nullable=False)
-    image_id = db.Column(db.Integer, db.ForeignKey('image.id'))
-    description = db.Column(db.Text, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('dish_category.id'))  # pylint: disable=E1101
+    dish_name = db.Column(db.String(100), unique=True, nullable=False)  # pylint: disable=E1101
+    cost_price = db.Column(db.Integer, nullable=False)  # pylint: disable=E1101
+    value_added = db.Column(db.Integer, nullable=False)  # pylint: disable=E1101
+    image_id = db.Column(db.Integer, db.ForeignKey('image.id'))  # pylint: disable=E1101
+    description = db.Column(db.Text, nullable=False)  # pylint: disable=E1101
 
 
 class Status(db.Model):  # pylint: disable=too-few-public-methods
@@ -90,8 +90,8 @@ class Status(db.Model):  # pylint: disable=too-few-public-methods
     status category table model
     """
     __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    status_name = db.Column(db.String(100), unique=True)
+    id = db.Column(db.Integer, primary_key=True)  # pylint: disable=E1101
+    status_name = db.Column(db.String(100), unique=True)  # pylint: disable=E1101
 
 
 class Delivery(db.Model):  # pylint: disable=too-few-public-methods
@@ -99,13 +99,13 @@ class Delivery(db.Model):  # pylint: disable=too-few-public-methods
     delivery category table model
     """
     __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    department_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=False)
-    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    courier_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
-    date = db.Column(db.Date, default=func.now())
-    customer_address = db.Column(db.Text, nullable=False)
-    status_id = db.Column(db.Integer, db.ForeignKey('status.id'))
+    id = db.Column(db.Integer, primary_key=True)  # pylint: disable=E1101
+    department_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=False)  # pylint: disable=E1101
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # pylint: disable=E1101
+    courier_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)  # pylint: disable=E1101
+    date = db.Column(db.Date, default=func.now())  # pylint: disable=E1101
+    customer_address = db.Column(db.Text, nullable=False)  # pylint: disable=E1101
+    status_id = db.Column(db.Integer, db.ForeignKey('status.id'))  # pylint: disable=E1101
 
 
 class Order(db.Model):  # pylint: disable=too-few-public-methods
@@ -113,7 +113,7 @@ class Order(db.Model):  # pylint: disable=too-few-public-methods
     order category table model
     """
     __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    delivery_id = db.Column(db.Integer, db.ForeignKey('delivery.id'), nullable=True)
-    dish_id = db.Column(db.Integer, db.ForeignKey('dish.id'), nullable=False)
-    status_id = db.Column(db.Integer, db.ForeignKey('status.id'))
+    id = db.Column(db.Integer, primary_key=True)  # pylint: disable=E1101
+    delivery_id = db.Column(db.Integer, db.ForeignKey('delivery.id'), nullable=True)  # pylint: disable=E1101
+    dish_id = db.Column(db.Integer, db.ForeignKey('dish.id'), nullable=False)  # pylint: disable=E1101
+    status_id = db.Column(db.Integer, db.ForeignKey('status.id'))  # pylint: disable=E1101
